cc69d845f3ed53aec53c4182f5c1968303898958
Drew Peterson <drewpeterson@Drews-MacBook-Pro.local>
2025-08-26 19:51:58 -0700

    Add rack-cors gem, fixed the tracks controller to render index and with render json

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -66,6 +66,7 @@ class ArtistsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def artist_params
-      params.require(:artist).permit(:name, :priority_id, :phase_id, :complete, :wikipedia, :discogs)
+      params.require(:artist).permit(:name, :priority_id, :phase_id, :complete, :wikipedia, :discogs, :genre_id)
     end
+
 end
--- app/views/artists/_artist.json.jbuilder
+++ app/views/artists/_artist.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! artist, :id, :name, :priority_id, :phase_id, :complete, :wikipedia, :discogs, :created_at, :updated_at
+json.extract! artist, :id, :name, :genre_id, :priority_id, :phase_id, :complete, :wikipedia, :discogs, :created_at, :updated_at
 json.url artist_url(artist, format: :json)
